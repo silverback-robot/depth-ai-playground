aa457d9ed8fbedfe3a2a8fa5fb97ec748522f8a7
Harish Sundaresh <yesharish@yahoo.in>
2024-04-27 14:50:46 +1000

    stereo output with mono cams

--- src/main.py
+++ src/main.py
@@ -2,42 +2,78 @@ import depthai as dai
 import cv2
 import numpy as np
 
-pipeline = dai.Pipeline()
+def getFrame(queue):
+  # Get frame from queue
+  frame = queue.get()
+  # Convert frame to OpenCV format and return
+  return frame.getCvFrame()
 
-monoLeft = pipeline.createMonoCamera()
-monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
-monoRight = pipeline.createMonoCamera()
-monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+def getMonoCamera(pipeline, isLeft):
+    # Configure mono camera
+    mono = pipeline.createMonoCamera()
+ 
+    # Set Camera Resolution
+    mono.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+    if isLeft:
+        # Get left camera
+        mono.setBoardSocket(dai.CameraBoardSocket.LEFT)
+    else :
+        # Get right camera
+        mono.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+    return mono
 
-xoutLeft = pipeline.createXLinkOut()
-xoutLeft.setStreamName("left")
+if __name__ == '__main__':
+    pipeline = dai.Pipeline()
+ 
+    # Set up left and right cameras
+    monoLeft = getMonoCamera(pipeline, isLeft = True)
+    monoRight = getMonoCamera(pipeline, isLeft = False)
+ 
+    # Set output Xlink for left camera
+    xoutLeft = pipeline.createXLinkOut()
+    xoutLeft.setStreamName("left")
+ 
+    # Set output Xlink for right camera
+    xoutRight = pipeline.createXLinkOut()
+    xoutRight.setStreamName("right")
+  
+    # Attach cameras to output Xlink
+    monoLeft.out.link(xoutLeft.input)
+    monoRight.out.link(xoutRight.input)
 
-xoutRight = pipeline.createXLinkOut()
-xoutRight.setStreamName("right")
-
-monoLeft.out.link(xoutLeft.input)
-monoRight.out.link(xoutRight.input)
-
-with dai.Device(pipeline) as device:
-    leftQueue = device.getOutputQueue(name="left", maxSize=1)
-    leftFrame = leftQueue.get()
-
-    rightQueue = device.getOutputQueue(name="right", maxSize=1)
-    rightFrame = rightQueue.get()
-
-    cv2.namedWindow("Stereo Pair")
-    print(leftFrame.shape, rightFrame.shape)
-    sideBySide = True
-
-    while True:
-        if sideBySide:
-            imOut = np.hstack((leftFrame, rightFrame))
-        else:
-            imOut = np.uint8((leftFrame/2 + rightFrame/2))
+    with dai.Device(pipeline) as device:
+        # Get output queues. 
+        leftQueue = device.getOutputQueue(name="left", maxSize=1)
+        rightQueue = device.getOutputQueue(name="right", maxSize=1)
+    
+        # Set display window name
+        cv2.namedWindow("Stereo Pair")
+        # Variable used to toggle between side by side view and one frame view. 
+        sideBySide = True
+        while True:
+            # Get left frame
+            leftFrame = getFrame(leftQueue)
+            # Get right frame 
+            rightFrame = getFrame(rightQueue)
         
-        cv2.imshow("Stereo Pair", imOut)
-        key = cv2.waitKey(1)
-        if key == ord('q'):
-            break
-        elif key == ord('t'):
-            sideBySide = not sideBySide
+            if sideBySide:
+                # Show side by side view
+                imOut = np.hstack((leftFrame, rightFrame))
+            else : 
+                # Show overlapping frames
+                imOut = np.uint8(leftFrame/2 + rightFrame/2)
+        
+            # Display output image
+            cv2.imshow("Stereo Pair", imOut)
+            
+            # Check for keyboard input
+            key = cv2.waitKey(1)
+            if key == ord('q'):
+                # Quit when q is pressed
+                break
+            elif key == ord('t'):
+                # Toggle display when t is pressed
+                sideBySide = not sideBySide
+
+    cv2.destroyAllWindows()
+    
